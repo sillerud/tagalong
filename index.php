a4806abdf139d41ac1723ac4e9769448d0c93f3e
gjermundnor <gjermundnor@outlook.com>
2015-10-14 12:03:10 +0200

    Første index fil

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <link rel="stylesheet" type="text/css" href=".css">
+
+    <title>Test page</title>
+</head>
+<body>
+
+
+
+
+<h2>Test git</h2>
+
+<!-- jQuery library -->
+<script src="jquery-1.11.1.js"></script>
+<script src=".js"></script>
+
+</body>
+</html>
