cfe4b05f7c1f54328e7d9261274446516ee0d3c6
gjermundnor <gjermundnor@outlook.com>
2015-11-23 10:56:23 +0100

    statisk kodemiljø

---
+++ frontend/statisk/js/custom.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+
+
+    // Kjører hver gang vinduet endrer størrelse
+    $(window).resize(function() {
+
+    });
+
+
+// Hamburgerikon animasjon
+var menuOpen = false;
+$('.burger-wrap').on("click", function(){
+  if(menuOpen == false){
+    $('.menu').fadeIn();
+    $('.bar1').css({ transform: 'rotate(' + 45 + 'deg)' });
+    $('.bar1').css('top', '10px');
+    $('.bar2').css({ transform: 'rotate(' + -45 + 'deg)' });
+    $('.bar2').css('top', '10px');
+    $('.bar3').fadeOut();
+    menuOpen = true;
+  }else if(menuOpen == true){
+    $('.menu').fadeOut();
+    $('.bar1').css({ transform: 'rotate(' + 0 + 'deg)' });
+    $('.bar1').css('top', '0px');
+    $('.bar2').css({ transform: 'rotate(' + 0 + 'deg)' });
+    $('.bar2').css('top', '20px');
+    $('.bar3').fadeIn();
+    menuOpen = false;
+  }
+});
+// END hamburgerikon animasjon
+
+}); // END DOM
