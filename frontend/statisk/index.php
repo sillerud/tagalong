e43b71a17160e7af0708a5c2fc1a93366931e0a1
gjermundnor <gjermundnor@outlook.com>
2015-11-23 11:07:38 +0100

    La inn noen filer

---
+++ frontend/statisk/index.php
@@ -0,0 +1,8 @@
+<?php include 'header.php'; ?>
+
+<div class="container">
+    <!-- Kode her -->
+    hei
+</div>
+
+<?php include 'footer.php'; ?>
---
+++ frontend/statisk/sidebar.php
@@ -0,0 +1,13 @@
+<?php include 'header.php'; ?>
+
+<div class="container">
+    <div class="col-sm-4">
+
+
+    </div>
+    <div class="col-sm-8">
+        FEED
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
