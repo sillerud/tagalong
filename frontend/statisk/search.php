5708d87de893dbf643df67005d7f5f3cb30ef10f
samuelloken <samuel_960@msn.com>
2015-11-23 14:30:34 +0100

    delvis kodet search people og prøvd å finne en løsning til tabs i search

---
+++ frontend/statisk/search.php
@@ -0,0 +1,60 @@
+<?php include 'header.php'; ?>
+
+<div class="container testboks">
+    <!-- Kode her -->
+    <!-- Kilde til tab-koden fra: http://codepen.io/wallaceerick/pen/ojtal -->
+    <h1> Search </h1>
+        
+             
+
+<ul class="tabs">
+    <li>
+        <input type="radio" name="tabs" id="tab1" checked />
+        <label for="tab1">#tags</label>
+        <div id="tab-content1" class="tab-content">
+          <p>"Lorem ipsum dolor ."</p>
+        </div>
+    </li>
+  
+    <li>
+        <input type="radio" name="tabs" id="tab2" />
+        <label for="tab2">Events</label>
+        <div id="tab-content2" class="tab-content">
+          <p>"Sed ut" </p>
+        </div>
+    </li>
+    
+     <li>
+        <input type="radio" name="tabs" id="tab3" />
+        <label for="tab2">Pages</label>
+        <div id="tab-content2" class="tab-content">
+          <p>"Sed ut" </p>
+        </div>
+    </li>
+    
+     <li>
+        <input type="radio" name="tabs" id="tab4" />
+        <label for="tab2">Groups</label>
+        <div id="tab-content2" class="tab-content">
+          <p>"Sed ut" </p>
+        </div>
+    </li>
+    
+     <li>
+        <input type="radio" name="tabs" id="tab5" />
+        <label for="tab2">People</label>
+        <div id="tab-content2" class="tab-content">
+          <p>"Sed ut" </p>
+        </div>
+    </li>
+</ul>
+
+<br style="clear: both;" />
+
+
+    </div>
+    
+    <?php include "search_people.php"; ?>
+</div>
+
+<?php include 'footer.php'; ?>
---
+++ frontend/statisk/search_people.php
@@ -0,0 +1,9 @@
+<div class="blue search-people">
+        <img src="images/olaNordmann.jpeg" class="search-profilbilde" alt="Profilbilde">
+        <div class="search-people-wrap">
+        <h4>Ola Nordmann</h4>
+        <p>Linje: interaktiv design</p>
+        <p>Interested in sports</p>
+    </div>
+        
+</div>
