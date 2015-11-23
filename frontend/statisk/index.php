53bed6f998695ca88c98a16f9cfc0c86b0a16f35
gjermundnor <gjermundnor@outlook.com>
2015-11-23 11:43:02 +0100

    Oppdatert CSS med westerdals farger og bodyfarge

--- frontend/statisk/index.php
+++ frontend/statisk/index.php
@@ -1,8 +1,13 @@
 <?php include 'header.php'; ?>
 
 <div class="container">
-    <!-- Kode her -->
-    hei
-</div>
+
+    <div class="col-sm-3">
+        <div class="card-wrap">
+            <h3>Tittel på Card</h3>
+        </div>
+    </div>
+
+</div><!-- END - container -->
 
 <?php include 'footer.php'; ?>
