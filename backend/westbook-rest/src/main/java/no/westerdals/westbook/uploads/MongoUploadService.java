1d253d9c9febdb667dd0fc055209ea6a43353a14
Kevin Sillerud <silkev14@student.westerdals.no>
2015-11-24 17:42:22 +0100

    The mongodb upload implementation should be fully functioning and uses its file meta features to store dates and names

--- backend/westbook-rest/src/main/java/no/westerdals/westbook/uploads/MongoUploadService.java
+++ backend/westbook-rest/src/main/java/no/westerdals/westbook/uploads/MongoUploadService.java
@@ -1,10 +1,9 @@
 package no.westerdals.westbook.uploads;
 
+import com.mongodb.DBObject;
 import com.mongodb.gridfs.GridFSDBFile;
 import com.mongodb.gridfs.GridFSFile;
 import no.westerdals.westbook.model.FileMeta;
-import org.bson.types.ObjectId;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -32,28 +31,34 @@ public class MongoUploadService implements UploadService
         GridFSDBFile file = fsTemplate.findOne(Query.query(Criteria.where("_id").is(id)));
         if (file == null)
             return null;
-
-        FileMeta fileMeta = mappingConverter.read(FileMeta.class, file.getMetaData());
-        fileMeta.setName(file.getFilename());
-        fileMeta.setUploadTime(file.getUploadDate());
-
-        return new DownloadRequest(file.getInputStream(), out, fileMeta);
+        return new DownloadRequest(file.getInputStream(), out, deserialize(file));
     }
 
     @Override
     public FileMeta uploadFile(FileMeta meta, InputStream in) throws IOException
     {
+        String name = meta.getName();
         meta.setName(null);
         meta.setUploadTime(null);
-        GridFSFile file = fsTemplate.store(in, meta.getName(), meta);
-        FileMeta result = mappingConverter.read(FileMeta.class, file.getMetaData());
-        result.setId(file.getId().toString());
-        return result;
+        GridFSFile file = fsTemplate.store(in, name, meta);
+        return deserialize(file);
     }
 
     @Override
     public FileMeta getFileMeta(String id)
     {
         return mappingConverter.read(FileMeta.class, fsTemplate.findOne(Query.query(Criteria.where("_id").is(id))));
+    }
+
+    private FileMeta deserialize(GridFSFile file)
+    {
+        DBObject meta = file.getMetaData();
+        FileMeta fileMeta = new FileMeta(
+                (String)meta.get("ownerId"),
+                file.getFilename(),
+                (boolean)meta.get("attachment"),
+                file.getUploadDate());
+        fileMeta.setId(file.getId().toString());
+        return fileMeta;
     }
 }
