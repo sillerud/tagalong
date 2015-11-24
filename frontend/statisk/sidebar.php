51faf909a5c35313eb61fbb0288b5287c5f40dc4
Maane Arab <maansado@hotmail.com>
2015-11-24 15:08:46 +0100

    Fikser på boks

--- frontend/statisk/sidebar.php
+++ frontend/statisk/sidebar.php
@@ -20,24 +20,30 @@
         
 
     </div><!-- END sidebar -->
-    
     <div class="col-sm-8">
         <h2>Feed</h2>
         
         <div class="inner-feedbar">
             
-            <div class="col-feedbar-box">   
+        <div class="col-feedbar-box"> 
+            
+            <div class="boks">
+                <div class="col-sirkel"></div>
+                <div class="col-feed-box"></div>
             </div>
             
-            <div class="col-feed-box">
+                    <div class="boks">
+                <div class="col-sirkel"></div>
+                <div class="col-feed-box"></div>
             </div>
+        </div>  
             
-            <div class="col-feed-box">
-            </div>
+        </div>
             
-            <div class="col-feed-box">
-            </div>
-        <!--- end feed-->
+    </div>
+        
+        
+        
     </div>
 </div><!-- END container -->
 
