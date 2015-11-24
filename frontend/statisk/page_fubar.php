c4b2e7585a6cd2b60ef42e93456b607d1a8266dc
hannahsollund <hannahlizj@gmail.com>
2015-11-24 15:46:56 +0100

    laget fubar page

---
+++ frontend/statisk/page_fubar.php
@@ -0,0 +1,32 @@
+
+<link rel="stylesheet"  href="css/page_fubar.css" type="text/css">
+
+
+<?php include 'header.php'; ?>
+
+<div class="container">
+    
+    <div class="col-12">
+         <div a href="#" class="logo"></div>
+         
+         <h2>Page: fuBar</h2>
+        
+        <h4>Location:</h4>
+        
+        <h4 href="#">Facebook</h4>
+        
+        <div class="strek"></div>
+        <div class="strek"></div>
+        
+        <div class="upcoming-events-boks">
+           <h3>Upcoming events</h3>
+        </div>
+        
+        <div class="recent-posts-boks">
+           <h3>Recent posts</h3>
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
+
