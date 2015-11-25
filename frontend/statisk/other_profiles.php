910d812ccd0e20c9a0b0c9335b9614eec824668d
reinole <reinhartsen.oc@gmail.com>
2015-11-25 16:51:52 +0100

    -added feed box

--- frontend/statisk/other_profiles.php
+++ frontend/statisk/other_profiles.php
@@ -10,32 +10,41 @@
         <div class="info-holder">
             <h2 class="name">Lise Nordmann</h2>
 
-            <div class="age-holder">
+            <div class="holder">
                 <p class="age"><b>Age:</b></p>
                 <p class="age-c">23</p>
             </div>
 
-            <div class="gender-holder">
-                <p class="gender"><b>Gender: </b></p>
+            <div class="holder">
+                <p class="gender"><b>Gender:</b></p>
                 <p class="gender-c">Female</p>
             </div>
-            <div class="city-holder">
+            <div class="holder">
                 <p class="city"><b>City of residence: </b></p>
                 <p class="city-c">Oslo</p>
             </div>
-            <div class="field-of-study-holder">
+            <div class="holder">
                 <p class="field-of-study"><b>Field of study: </b></p>
                 <p class="field-of-study-c">3D Design</p>
             </div>
-            <div class="contact-info-holder">
+            <div class="holder">
                 <p class="contact-info"><b>Contact info: </b></p>
                 <p class="contact-info-c">norlis14@student.westerdals.no</p>
             </div>
-            <div class="interests-holder">
+            <div class="holder">
                 <p class="interests"><b>Interests: </b></p>
                 <p class="interests-c"> 3ds Max</p>
             </div>
+            <div class="holder">
+                <p class="about-me"><b>About me:</b></p>
+                <div clas
+            </div>
         </div>
+    </div>
+
+    <div class="profile-post-box">
+
+
     </div>
 
     <div class="a">
