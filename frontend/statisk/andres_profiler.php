5c01e9388b1cd622e297b8ad2700e46f82377726
reinole <reinhartsen.oc@gmail.com>
2015-11-25 14:55:43 +0100

    -lagt til other_profiles.css and added it in header. added andres_profiler.php

---
+++ frontend/statisk/andres_profiler.php
@@ -0,0 +1,22 @@
+<?php include 'header.php'; ?>
+
+<div class="container">
+    <!-- Kode her -->
+    <!--<div a href="#" class="logo"></div>
+
+    <h2 class="page-header">Lise Nordmann</h2>-->
+
+    <div class="profile-box">
+
+    </div>
+
+    <div class="upcoming-events-box">
+
+    </div>
+
+    <div class="recent-posts-box">
+
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
