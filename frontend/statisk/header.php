316db46ed6e304260ddc12d87fc7451a35a4fe9e
gjermundnor <gjermundnor@outlook.com>
2015-11-25 15:22:54 +0100

    Lagt til stylesheet for profil_edit.php + profil_edit .php og .css

--- frontend/statisk/header.php
+++ frontend/statisk/header.php
@@ -14,9 +14,10 @@
     <link rel="stylesheet" type="text/css" href="css/sidebar.css">
     <link rel="stylesheet" type="text/css" href="css/page_fubar.css">
     <link rel="stylesheet" type="text/css" href="css/other_profiles.css">
+    <link rel="stylesheet" type="text/css" href="css/profil_edit.css">
     <link rel="stylesheet" href="../bower_components/components-font-awesome/css/font-awesome.css"><!-- Font Awesome ikoner -->
 
-    <title>Test page</title>
+    <title>Tag along</title>
 </head>
 <body>
 
