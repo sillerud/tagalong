5dda5cd78561776489ea3a47bc387693ba889655
Maane Arab <maansado@hotmail.com>
2015-11-25 16:44:36 +0100

    gjort ferdig feed

--- frontend/statisk/sidebar.php
+++ frontend/statisk/sidebar.php
@@ -1,50 +1,61 @@
-<?php include 'header.php'; ?>
+    <?php include 'header.php'; ?>
 
-<div class="container">
-    <div class="col-sm-4 sidebar">
-        <h2>Sidebar</h2>
-        
-        <div class="inner-sidebar">
-            
-            <div class="col-sidebar-cards">
-                <h5>Social</h5>   
-            </div>
-            <div class="col-sidebar-cards">
-                <h5>Fubar</h5>    
-            </div>
-             <div class="col-sidebar-cards">
-            <h5>Academic interests</h5>    
-            </div>
-            
-        </div><!-- END - inner-sidebar -->
-        
+    <div class="container">
+        <div class="col-sm-4 sidebar">
+            <h2>Sidebar</h2>
 
-    </div><!-- END sidebar -->
-    <div class="col-sm-8">
-        <h2>Feed</h2>
-        
-        <div class="inner-feedbar">
-            
-        <div class="col-feedbar-box"> 
-            
-            <div class="boks">
-                <div class="col-sirkel"></div>
-                <div class="col-feed-box"></div>
+            <div class="inner-sidebar">
+
+                <div class="col-sidebar-cards">
+                    <h5>Social</h5>   
+                </div>
+                <div class="col-sidebar-cards">
+                    <h5>Fubar</h5>    
+                </div>
+                 <div class="col-sidebar-cards">
+                <h5>Academic interests</h5>    
+                </div>
+
+            </div><!-- END - inner-sidebar -->
+
+
+        </div><!-- END sidebar -->
+        <div class="col-sm-8">
+            <h2>Feed</h2>
+
+            <div class="inner-feedbar">    
                 
-            </div>
+                <div class="col-feedbar-box"> 
+                    
                     <div class="boks">
-                <div class="col-sirkel"></div>
-                <div class="col-feed-box"></div>
-            </div>
-        </div>  
-            
-        </div>
-            
-    </div>
-        
-        
-        
-    </div>
-</div><!-- END container -->
+                        <img src="images/olaNordmann.jpeg" class="col-sirkel">
+                        <div class="feed-boks-innhold">
+                            <h3 class="feed-boks-tittel">Tittel</h3>  
+                            <h6 class="ekstra-info">Dato: 25.11.2015 - Ola Nordmann</h6>                    
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                        </div>
+                    </div><!-- END boks -->
+                    
+                    <div class="boks">
+                        <img src="images/olaNordmann.jpeg" class="col-sirkel">
+                        <div class="feed-boks-innhold">
+                            <h3 class="feed-boks-tittel">Tittel</h3>  
+                            <h6 class="ekstra-info">Dato: 25.11.2015 - Ola Nordmann</h6>                    
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                        </div>
+                    </div><!-- END boks -->
+                    
+                </div><!-- END feedbar-box -->
+                
+                
 
-<?php include 'footer.php'; ?>
+            </div><!-- END inner-feedbar -->
+
+        </div><!-- END col-sm-8 -->
+
+
+
+        </div>
+    </div><!-- END container -->
+
+    <?php include 'footer.php'; ?>
