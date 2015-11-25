f6c385e0a97956ac2e4328cd4cb7c2bfe94d06f3
reinole <reinhartsen.oc@gmail.com>
2015-11-25 17:34:29 +0100

    -profile picture added

--- frontend/statisk/other_profiles.php
+++ frontend/statisk/other_profiles.php
@@ -6,48 +6,55 @@
 
     <h2 class="page-header">Lise Nordmann</h2>
 
-    <div class="col-profile-box">
-        <div class="info-holder">
-            <h2 class="name">Lise Nordmann</h2>
+    <div class="profile-box">
+         <div class="profile-pic">
+            <img src="images/olaNordmann.jpeg" class="profile-pic-holder">
+        </div>
+            <div class="info-holder">
+                <h2 class="name">Lise Nordmann</h2>
 
-            <div class="holder">
-                <p class="age"><b>Age:</b></p>
-                <p class="age-c">23</p>
-            </div>
+                <div class="holder">
+                    <p class="age"><b>Age:</b></p>
+                    <p class="age-c">23</p>
+                </div>
 
-            <div class="holder">
-                <p class="gender"><b>Gender:</b></p>
-                <p class="gender-c">Female</p>
-            </div>
-            <div class="holder">
-                <p class="city"><b>City of residence: </b></p>
-                <p class="city-c">Oslo</p>
-            </div>
-            <div class="holder">
-                <p class="field-of-study"><b>Field of study: </b></p>
-                <p class="field-of-study-c">3D Design</p>
-            </div>
-            <div class="holder">
-                <p class="contact-info"><b>Contact info: </b></p>
-                <p class="contact-info-c">norlis14@student.westerdals.no</p>
-            </div>
-            <div class="holder">
-                <p class="interests"><b>Interests: </b></p>
-                <p class="interests-c"> 3ds Max</p>
-            </div>
-            <div class="holder">
-                <p class="about-me"><b>About me:</b></p>
+                <div class="holder">
+                    <p class="gender"><b>Gender:</b></p>
+                    <p class="gender-c">Female</p>
+                </div>
+                <div class="holder">
+                    <p class="city"><b>City of residence: </b></p>
+                    <p class="city-c">Oslo</p>
+                </div>
+                <div class="holder">
+                    <p class="field-of-study"><b>Field of study: </b></p>
+                    <p class="field-of-study-c">3D Design</p>
+                </div>
+                <div class="holder">
+                    <p class="contact-info"><b>Contact info: </b></p>
+                    <p class="contact-info-c">norlis14@student.westerdals.no</p>
+                </div>
+                <div class="holder">
+                    <p class="interests"><b>Interests: </b></p>
+                    <p class="interests-c"> 3ds Max</p>
+                </div>
+                <div class="holder">
+                    <p class="about-me"><b>About me:</b></p>
 
-            </div>
-            <div class="about-me-box">
-                <p class="about-me-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+                </div>
+                <div class="about-me-box">
+                    <p class="about-me-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
                     Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
 
+                </div>
             </div>
-        </div>
     </div>
     <div class="profile-posts">Lise's posts</div>
     <div class="profile-post-box"
+        <div class="profile-post">
+
+        </div>
+
 
     </div>
 </div>
