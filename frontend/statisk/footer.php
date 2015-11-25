f10a0d9d97406416f0f36be07e07487f35c79085
gjermundnor <gjermundnor@outlook.com>
2015-11-25 21:49:41 +0100

    Oversikt over utviklede sider i footer

--- frontend/statisk/footer.php
+++ frontend/statisk/footer.php
@@ -1,2 +1,20 @@
+<footer>
+    <div class="container">
+    <strong>Link til "alle" sidene:</strong>
+        <a href="index.php">Forside</a> -
+        <a href="other_profiles.php">Andre sin profil</a> -
+        <a href="profil_edit.php">Rediger sin egen profil</a> -
+        <a href="page_fubar.php">Page (fubar)</a> -
+        <a href="search.php">Search</a> -
+        <a href="sidebar.php">Feed + sidebar</a>
+    </div>
+
+</footer>
+<style>
+footer{
+    padding: 100px 0 30px 0;
+}
+</style>
+
 <script src="../bower_components/jquery/dist/jquery.js"></script>
 <script src="js/custom.js"></script>
