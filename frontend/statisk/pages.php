fd995c9e62df02e78849d904ceb3fee6c38feffd
hannahsollund <hannahlizj@gmail.com>
2015-11-26 12:13:47 +0100

    endring

--- frontend/statisk/pages.php
+++ frontend/statisk/pages.php
@@ -6,11 +6,25 @@
 
     <div class="pages-main-content">
         
-        <div class="pages-result-wrap">
+        <div class="pages-most-popular-wrap pages-content-box">
+           
+           <h3 class="pages-most-popular-header">Most popular</h3>
+           
             <img src="images/olaNordmann.jpeg" class="pages-result-profilbilde" alt="Profilbilde">
-            <h4 class="pages-result-title">Fubar</h4>
-            <p class ="page-result-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus numquam nam neque consectetur perferendis? Animi esse laborum, accusamus, molestiae, nobis ipsa doloribus eaque at quibusdam id eligendi. Temporibus, accusantium, voluptatibus?</p>
-        </div><!--end pages-result-content-->
+            <h4 class="page-site-title">Fubar</h4>
+            <p class ="page-results-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus numquam nam neque consectetur perferendis? Animi esse laborum, accusamus, molestiae, nobis ipsa doloribus eaque at quibusdam id eligendi. Temporibus, accusantium, voluptatibus?</p>
+            
+        </div><!--end pages-most-popular-wrap-->
+        
+        <div class="pages-all-pages-wrap pages-content-box">
+           
+           <h3 class="pages-all-pages-header">All pages</h3>
+           
+            <img src="images/olaNordmann.jpeg" class="pages-result-profilbilde" alt="Profilbilde">
+            <h4 class="page-site-title">Fubar</h4>
+            <p class ="page-results-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus numquam nam neque consectetur perferendis? Animi esse laborum, accusamus, molestiae, nobis ipsa doloribus eaque at quibusdam id eligendi. Temporibus, accusantium, voluptatibus?</p>
+            
+        </div><!--end pages-all-results-wrap-->
         
     </div><!--end pages-main-content-->
 
