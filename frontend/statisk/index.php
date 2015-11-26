7b46d1c8e275afd1257ce0b8c6f3e109ec9e2d2e
gjermundnor <gjermundnor@outlook.com>
2015-11-26 11:05:41 +0100

    Endret tekst på noen cards + legg til nytt card card

--- frontend/statisk/index.php
+++ frontend/statisk/index.php
@@ -5,7 +5,7 @@
         <div class="col-sm-4">
             <div class="card-wrap">
                 <div class="new-post-in-card-icon">
-                    99
+                    5
                 </div>
                 <div class="edit-card-icon">
                     <i class="fa fa-wrench"></i>
@@ -25,6 +25,40 @@
                                     <li>- - #Utviling</li>
                                 </ul>
                             </li>
+                            <li>@Fubar
+                                <ul class="tag-path">
+                                    <li>@Fubar</li>
+                                </ul>
+                            </li>
+                            <li>#Sosialt
+                                <ul class="tag-path">
+                                    <li>#Interaktivt design</li>
+                                    <li>- #Web</li>
+                                    <li>- - #Utviling</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div><!-- END - card-wrap -->
+        </div><!-- END col-sm-4 -->
+
+        <div class="col-sm-4">
+            <div class="card-wrap">
+                <div class="new-post-in-card-icon">
+                    20
+                </div>
+                <div class="edit-card-icon">
+                    <i class="fa fa-wrench"></i>
+                </div>
+                <div class="card-wrap-inner">
+
+                    <h3>Social</h3>
+                    <p>Alt sosialt</p>
+
+                    <!-- Viser kun 3 tags, kan sepsifisere hvilke det er i edit card -->
+                    <div class="card-tag-wrap">
+                        <ul class="card-tag-list">
                             <li>@Fubar
                                 <ul class="tag-path">
                                     <li>@Fubar</li>
@@ -87,29 +121,29 @@
         <div class="col-sm-4">
             <div class="card-wrap">
                 <div class="new-post-in-card-icon">
-                    99
+                    0
                 </div>
                 <div class="edit-card-icon">
                     <i class="fa fa-wrench"></i>
                 </div>
                 <div class="card-wrap-inner">
 
-                    <h3>Tittel på Card</h3>
+                    <h3>Film ++</h3>
                     <p>Dette kortet er sosialt og om webutvikling</p>
 
                     <!-- Viser kun 3 tags, kan sepsifisere hvilke det er i edit card -->
                     <div class="card-tag-wrap">
                         <ul class="card-tag-list">
-                            <li>#Utvikling
+                            <li>#Render
                                 <ul class="tag-path">
-                                    <li>#Interaktivt design</li>
-                                    <li>- #Web</li>
-                                    <li>- - #Utviling</li>
+                                    <li>#Film&TV</li>
+                                    <li>- #Film</li>
+                                    <li>- - #Render</li>
                                 </ul>
                             </li>
-                            <li>@Fubar
+                            <li>#TV
                                 <ul class="tag-path">
-                                    <li>@Fubar</li>
+                                    <li>TV</li>
                                 </ul>
                             </li>
                             <li>#Sosialt
@@ -126,43 +160,11 @@
         </div><!-- END col-sm-4 -->
 
         <div class="col-sm-4">
-            <div class="card-wrap">
-                <div class="new-post-in-card-icon">
-                    99
-                </div>
-                <div class="edit-card-icon">
-                    <i class="fa fa-wrench"></i>
-                </div>
-                <div class="card-wrap-inner">
-
-                    <h3>Tittel på Card</h3>
-                    <p>Dette kortet er sosialt og om webutvikling</p>
-
-                    <!-- Viser kun 3 tags, kan sepsifisere hvilke det er i edit card -->
-                    <div class="card-tag-wrap">
-                        <ul class="card-tag-list">
-                            <li>#Utvikling
-                                <ul class="tag-path">
-                                    <li>#Interaktivt design</li>
-                                    <li>- #Web</li>
-                                    <li>- - #Utviling</li>
-                                </ul>
-                            </li>
-                            <li>@Fubar
-                                <ul class="tag-path">
-                                    <li>@Fubar</li>
-                                </ul>
-                            </li>
-                            <li>#Sosialt
-                                <ul class="tag-path">
-                                    <li>#Interaktivt design</li>
-                                    <li>- #Web</li>
-                                    <li>- - #Utviling</li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+            <div class="card-wrap-add">
+                <div class="card-add-inner">
+                    <i class="fa fa-plus"></i><br>
+                    Add new card
+                </div><!-- END - card-add-inner -->
             </div><!-- END - card-wrap -->
         </div><!-- END col-sm-4 -->
 
