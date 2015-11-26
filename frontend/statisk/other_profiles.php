33bb1ca7456e21fa5a7b5f79bf6a2cceca5c9cff
reinole <reinhartsen.oc@gmail.com>
2015-11-26 11:29:51 +0100

    -done profile posts. made frames auto height

--- frontend/statisk/other_profiles.php
+++ frontend/statisk/other_profiles.php
@@ -5,8 +5,10 @@
     <div a href="#" class="logo"></div>
 
     <h2 class="page-header">Lise Nordmann</h2>
+    <!--<a href="profil_edit.php" class="edit-profile-page">Edit your profile</a>
 <!-- Profile first frame-->
     <div class="profile-box">
+    <a href="profil_edit.php" class="edit-profile-page">Edit</a>
         <!-- Profile picture div -->
          <div class="profile-pic">
             <img src="images/olaNordmann.jpeg" class="profile-pic-holder">
@@ -16,28 +18,28 @@
                 <h2 class="name">Lise Nordmann</h2>
 
                 <div class="holder">
-                    <p class="age"><b>Age:</b></p>
+                    <p class="info-format" class="age" ><b>Age:</b></p>
                     <p class="age-c">23</p>
                 </div>
 
                 <div class="holder">
-                    <p class="gender"><b>Gender:</b></p>
+                    <p class="info-format" class="gender" ><b>Gender:</b></p>
                     <p class="gender-c">Female</p>
                 </div>
                 <div class="holder">
-                    <p class="city"><b>City of residence: </b></p>
+                    <p class="info-format" class="city"><b>City of residence: </b></p>
                     <p class="city-c">Oslo</p>
                 </div>
                 <div class="holder">
-                    <p class="field-of-study"><b>Field of study: </b></p>
+                    <p class="info-format" class="field-of-study"><b>Field of study: </b></p>
                     <p class="field-of-study-c">3D Design</p>
                 </div>
                 <div class="holder">
-                    <p class="contact-info"><b>Contact info: </b></p>
+                    <p class="info-format" class="contact-info"><b>Contact info: </b></p>
                     <p class="contact-info-c">norlis14@student.westerdals.no</p>
                 </div>
                 <div class="holder">
-                    <p class="interests"><b>Interests: </b></p>
+                    <p class="info-format" class="interests"><b>Interests: </b></p>
                     <p class="interests-c"> 3ds Max</p>
                 </div>
                 <div class="holder">
@@ -55,15 +57,38 @@
     <!-- Profile first frame END -->
     <!-- Profile post START -->
     <div class="profile-posts">Lise's posts</div>
+
     <div class="profile-post-box"
-        <div class="col-feedbar-box" class="inner-feedbar">
-            <div class="boks">
-            <img src="images/olaNordmann.jpeg" class="col-sirkel">
+        <div class="col-feedbar-box" class="inner-feedbar" >
+            <div class="boks" >
+                <img src="images/olaNordmann.jpeg" class="col-sirkel">
                 <h3 class="feed-boks-tittel">Tittel</h3>
                 <h6 class="ekstra-info">Dato: 25.11.2015 - Ola Nordmann</h6>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    <div class="post-profile">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    </div>
             </div>
+            <div class="boks" >
+                <img src="images/olaNordmann.jpeg" class="col-sirkel">
+                <h3 class="feed-boks-tittel">Tittel</h3>
+                <h6 class="ekstra-info">Dato: 25.11.2015 - Ola Nordmann</h6>
+                    <div class="post-profile">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    </div>
+            </div>
+            <div class="boks" >
+                <img src="images/olaNordmann.jpeg" class="col-sirkel">
+                <h3 class="feed-boks-tittel">Tittel</h3>
+                <h6 class="ekstra-info">Dato: 25.11.2015 - Ola Nordmann</h6>
+                    <div class="post-profile">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    </div>
+            </div>
+
+
         </div>
 
 
