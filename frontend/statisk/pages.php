1d8360f8530e02855ccc15358010a4d16e576c33
hannahsollund <hannahlizj@gmail.com>
2015-11-26 11:52:07 +0100

    endring

--- frontend/statisk/pages.php
+++ frontend/statisk/pages.php
@@ -9,10 +9,10 @@
         <div class="pages-result-wrap">
             <img src="images/olaNordmann.jpeg" class="pages-result-profilbilde" alt="Profilbilde">
             <h4 class="pages-result-title">Fubar</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus numquam nam neque consectetur perferendis? Animi esse laborum, accusamus, molestiae, nobis ipsa doloribus eaque at quibusdam id eligendi. Temporibus, accusantium, voluptatibus?</p>
-        </div>
+            <p class ="page-result-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus numquam nam neque consectetur perferendis? Animi esse laborum, accusamus, molestiae, nobis ipsa doloribus eaque at quibusdam id eligendi. Temporibus, accusantium, voluptatibus?</p>
+        </div><!--end pages-result-content-->
         
-    </div>
+    </div><!--end pages-main-content-->
 
 </div><!--end container-->
 
