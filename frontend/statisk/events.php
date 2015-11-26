0753da0bec90f1fde109417136292d9d9d19a4d0
samuelloken <samuel_960@msn.com>
2015-11-26 11:41:19 +0100

    startet på events

---
+++ frontend/statisk/events.php
@@ -0,0 +1,24 @@
+<?php include 'header.php'; ?>
+
+<div class="container">
+    <h2>Events</h2>
+        
+   
+    
+    <div class="col-sm-6">
+        <div class="events-box">
+            <h5>My Events</h5>
+        </div><!--end my-events-box-->
+    </div>
+
+    <div class="col-sm-6">
+        <div class="events-box">
+           <h5>Upcoming Events</h5>
+        </div>
+    </div><!--end -->
+       
+         
+</div><!--end container-->
+
+<?php include 'footer.php'; ?>
+
