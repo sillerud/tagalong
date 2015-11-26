1cfcb6a1293f1122cc975ea2741b1fb5905038a3
Maane Arab <maansado@hotmail.com>
2015-11-26 15:30:14 +0100

    satt inn feeds

---
+++ frontend/statisk/movie_night_page.php
@@ -0,0 +1,37 @@
+<?php include 'header.php'; ?>
+
+<div class="container">
+    <!-- Kode her -->
+     <div class="movie-header-box">        
+       
+
+        <div class="movie-header-area">
+             <img src="images/fubar_logo.jpg" class="movie-bilde" alt="movie bilde">
+             <h2 class="movie-header-title">Movie night@Fubar</h2>
+
+           <p class="movie-description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
+</div><!--end page-header-area-->
+</div>
+    
+    <div class="move-content-feed">
+        
+   <div class="col-sm-4">
+       <h3 class="movie-box-title">
+       </h3>
+       
+       <div class="movie-upcoming-events-feed"></div>   
+</div>
+
+        <div class="col-sm-8">
+             <div class="boxinner-feedbar">
+            </div>
+            <h3 class="movie-recent-posts-title"></h3>
+ <div class="recent-movieposts-feed"></div>        
+        </div><!-- end recent-posts-box-->
+    
+    
+    </div><!---end content-feed-->
+    
+    
+</div><!--end container-->
+<?php include 'footer.php'; ?>
