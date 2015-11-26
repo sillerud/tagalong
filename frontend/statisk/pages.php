547f8a976128330fe22ed7911bec107ba2609193
hannahsollund <hannahlizj@gmail.com>
2015-11-26 12:52:58 +0100

    ferdig med pages

--- frontend/statisk/pages.php
+++ frontend/statisk/pages.php
@@ -6,25 +6,22 @@
 
     <div class="pages-main-content">
         
-        <div class="pages-most-popular-wrap pages-content-box">
-           
-           <h3 class="pages-most-popular-header">Most popular</h3>
-           
-            <img src="images/olaNordmann.jpeg" class="pages-result-profilbilde" alt="Profilbilde">
-            <h4 class="page-site-title">Fubar</h4>
-            <p class ="page-results-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus numquam nam neque consectetur perferendis? Animi esse laborum, accusamus, molestiae, nobis ipsa doloribus eaque at quibusdam id eligendi. Temporibus, accusantium, voluptatibus?</p>
-            
-        </div><!--end pages-most-popular-wrap-->
+           <div class="col-sm-6">
+              <div class="most-popular-pages">
+                   <h3 class="pages-most-popular-header">Most popular</h3>
+                    <img src="images/olaNordmann.jpeg" class="pages-result-profilbilde" alt="Profilbilde">
+                    <h4 class="page-site-title">Fubar</h4>
+                    <p class ="page-results-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus numquam nam neque consectetur perferendis?</p>
+                </div>
+            </div>
         
-        <div class="pages-all-pages-wrap pages-content-box">
-           
-           <h3 class="pages-all-pages-header">All pages</h3>
-           
-            <img src="images/olaNordmann.jpeg" class="pages-result-profilbilde" alt="Profilbilde">
-            <h4 class="page-site-title">Fubar</h4>
-            <p class ="page-results-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus numquam nam neque consectetur perferendis? Animi esse laborum, accusamus, molestiae, nobis ipsa doloribus eaque at quibusdam id eligendi. Temporibus, accusantium, voluptatibus?</p>
-            
-        </div><!--end pages-all-results-wrap-->
+           <div class="col-sm-6">
+              
+               <h3 class="pages-all-pages-header">All pages</h3>
+                <img src="images/olaNordmann.jpeg" class="pages-result-profilbilde" alt="Profilbilde">
+                <h4 class="page-site-title">Fubar</h4>
+                <p class ="page-results-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus numquam nam neque consectetur perferendis?</p>
+            </div>
         
     </div><!--end pages-main-content-->
 
