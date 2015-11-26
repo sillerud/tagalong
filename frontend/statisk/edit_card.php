9be35ccdd5789e615f1db6fda2ac8c21d6d54570
gjermundnor <gjermundnor@outlook.com>
2015-11-26 12:37:16 +0100

    add/edit card ferdig

--- frontend/statisk/edit_card.php
+++ frontend/statisk/edit_card.php
@@ -7,6 +7,9 @@
 
         <textarea class="edit-field3" rows="2" placeholder="Card description"></textarea>
 
+        <input class="edit-field2"  type="text" hidden><!-- Plassering av tags i form -->
+        <input class="edit-field2" placeholder="Legg til tags" type="search">
+
         <input type="submit" class="btn-fill" value="Save card">
     </form>
 </div>
