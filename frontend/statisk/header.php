be71a54135453c45512d1f7475a72bf1e5aa59ba
reinole <reinhartsen.oc@gmail.com>
2015-11-27 10:43:04 +0100

    -added nen_event.css to header.php

--- frontend/statisk/header.php
+++ frontend/statisk/header.php
@@ -17,6 +17,7 @@
     <link rel="stylesheet" type="text/css" href="css/other_profiles.css">
     <link rel="stylesheet" type="text/css" href="css/profil_edit.css">
     <link rel="stylesheet" type="text/css" href="css/events.css">
+    <link rel="stylesheet" type="text/css" href="css/new-events.css">
         <link rel="stylesheet" type="text/css" href="css/movie_night_page.css">
     <link rel="stylesheet" href="../bower_components/components-font-awesome/css/font-awesome.css"><!-- Font Awesome ikoner -->
 
