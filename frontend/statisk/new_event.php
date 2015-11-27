6cbd11000c212c542aefc025330c287c812fc01c
reinole <reinhartsen.oc@gmail.com>
2015-11-27 14:59:18 +0100

    -new event siden done.

--- frontend/statisk/new_event.php
+++ frontend/statisk/new_event.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 
-<!--Hovedkontainer START-->
-<div class="container new-event edit-fields" xmlns="http://www.w3.org/1999/html">
+
+<div class="container new-event edit-fields" xmlns="http://www.w3.org/1999/html"><!--Hovedkontainer START-->
     <center><h1>Create new event</h1></center>
 
     <div class="edit-profile-profile-pic">
@@ -28,33 +28,39 @@
                     </span>
                 </div>
             </div>
-
         </div>
+</div><!--Row END-->
 
         <div class="col-sm-12 ">
             <textarea rows="5" class="edit-field3" placeholder="Describe your event"></textarea>
         </div>
 
-        <div class="col-sm-12 invite-campus"
-            <form>
-                <p class="invite-text">Invite</p>
-                <input type="checkbox" name="Invite all campus"> Invite all subjects
-                <br>
-                <input type="checkbox" name="Campus Galleriet"> Teknologi/IT
-                <br>
-                <input type="checkbox" name="Campus Vaterland"> Ledelse
-                <br>
-                <input type="checkbox" name="Campus Galleriet"> Kommunikasjon
-                <br>
-                <input type="checkbox" name="Campus Galleriet"> Kunstfag
-                <br>
-                <input type="checkbox" name="Campus Galleriet"> Film, TV og spill
+        <div class="col-sm-12 invite-container">      <!--invite section-->
+             <div  class="invite-checkbox">
+                <form>
+                    <p>Invite</p>
+                    <input type="checkbox" name="Invite all campus"> Invite all subjects
+                    <br>
+                    <input type="checkbox" name="Campus Galleriet"> Teknologi/IT
+                    <br>
+                    <input type="checkbox" name="Campus Vaterland"> Ledelse
+                    <br>
+                    <input type="checkbox" name="Campus Galleriet"> Kommunikasjon
+                    <br>
+                    <input type="checkbox" name="Campus Galleriet"> Kunstfag
+                    <br>
+                    <input type="checkbox" name="Campus Galleriet"> Film, TV og spill
+                 </form>
+             </div>
 
-            </form>
-        <!--Row END-->
-    </div>
-    <!--Hovedkonteiner END-->
-</div>
+            <div class="invite-people">
+                <form>
+                    Invite individual people:
+                    <input type="search" name="googlesearch">
+                </form>
+            </div>
+        </div> <!-- Invite section-->
+</div><!--Hovedkonteiner END-->
 
 
 
