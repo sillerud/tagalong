6625e340044ceeffec8184798f1ead58b6962b5f
gjermundnor <gjermundnor@outlook.com>
2015-11-27 09:20:23 +0100

    Movie night ferdig

--- frontend/statisk/movie_night_page.php
+++ frontend/statisk/movie_night_page.php
@@ -2,36 +2,50 @@
 
 <div class="container">
     <!-- Kode her -->
-     <div class="movie-header-box">        
-       
+     <div class="movie-header-box">
+
 
         <div class="movie-header-area">
              <img src="images/fubar_logo.jpg" class="movie-bilde" alt="movie bilde">
              <h2 class="movie-header-title">Movie night@Fubar</h2>
 
            <p class="movie-description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
-</div><!--end page-header-area-->
-</div>
-    
-    <div class="move-content-feed">
-        
-   <div class="col-sm-4">
-       <h3 class="movie-box-title">
-       </h3>
-       
-       <div class="movie-upcoming-events-feed"></div>   
-</div>
+        </div><!--end page-header-area-->
+      </div>
+
+
+      <div class="row">
+       <div class="col-sm-4">
+           <h3 class="movie-box-title"></h3>
+
+           <div class="movie-upcoming-events-feed">
+              <div class="boks">
+                <h4>Tittel</h4>
+                <h5>Dato: 20.03.2015</h5>
+                <div class="btn-no-fill">Join</div>
+              </div>
+           </div>
+        </div>
 
         <div class="col-sm-8">
-             <div class="boxinner-feedbar">
-            </div>
+
             <h3 class="movie-recent-posts-title"></h3>
- <div class="recent-movieposts-feed"></div>        
+            <div class="recent-movieposts-feed">
+              <div class="boks">
+                        <img src="images/olaNordmann.jpeg" class="col-sirkel">
+                        <div class="feed-boks-innhold">
+                            <h3 class="feed-boks-tittel">Tittel</h3>
+                            <h6 class="ekstra-info">Dato: 25.11.2015 - Ola Nordmann</h6>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                        </div>
+                    </div><!-- END boks -->
+
+            </div>
         </div><!-- end recent-posts-box-->
-    
-    
-    </div><!---end content-feed-->
-    
-    
+    </div><!-- END row -->
+
+
+
+
 </div><!--end container-->
 <?php include 'footer.php'; ?>
