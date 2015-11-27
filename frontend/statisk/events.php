bf52bd5a239dce50a29e79e36758cdf4884bdfd1
gjermundnor <gjermundnor@outlook.com>
2015-11-27 10:53:44 +0100

    Event mini boks

--- frontend/statisk/events.php
+++ frontend/statisk/events.php
@@ -2,22 +2,53 @@
 
 <div class="container">
     <h2>Events</h2>
-        
-   
-    
-    <div class="col-sm-6">
-        <div class="events-box">
-            <h5>My Events</h5>
-        </div><!--end my-events-box-->
+
+
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="events-box">
+
+                <div class="an-event-wrap">
+                    <h4><strong>X-mas</strong></h4>
+                    <div class="an-event-date">24.12.2015</div>
+                    <div class="attending-or-not-wrap">
+                        <div class="attending-btn"><i class="fa fa-check"></i></div>
+                        <div class="not-attending-btn"><i class="fa fa-times"></i></div>
+                        <div class="number-attending">50 Attending</div>
+                    </div>
+                </div><!-- END - an-event-wrap -->
+
+                <div class="an-event-wrap">
+                    <h4><strong>New Years Eve</strong></h4>
+                    <div class="an-event-date">31.12.2015</div>
+                    <div class="attending-or-not-wrap">
+                        <div class="attending-btn"><i class="fa fa-check"></i></div>
+                        <div class="not-attending-btn"><i class="fa fa-times"></i></div>
+                        <div class="number-attending">128 Attending</div>
+                    </div>
+                </div><!-- END - an-event-wrap -->
+
+            </div><!--end my-events-box-->
+        </div>
+
+        <div class="col-sm-6">
+            <div class="events-box">
+
+               <div class="an-event-wrap">
+                    <h4><strong>January code session</strong></h4>
+                    <div class="an-event-date">10.01.2016</div>
+                    <div class="attending-or-not-wrap">
+                        <div class="attending-btn attending-checked"><i class="fa fa-check"></i></div>
+                        <div class="not-attending-btn"><i class="fa fa-times"></i></div>
+                        <div class="number-attending">50 Attending</div>
+                    </div>
+                </div><!-- END - an-event-wrap -->
+
+            </div>
+        </div><!--end -->
     </div>
 
-    <div class="col-sm-6">
-        <div class="events-box">
-           <h5>Upcoming Events</h5>
-        </div>
-    </div><!--end -->
-       
-         
+
 </div><!--end container-->
 
 <?php include 'footer.php'; ?>
