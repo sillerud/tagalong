276dea61760706bdb44653dc2d063c43b0249d65
Kevin Sillerud <silkev14@student.westerdals.no>
2016-03-02 11:42:04 +0100

    Fix logout

--- backend/westbook-rest/src/main/java/no/westerdals/westbook/config/SecurityConfig.java
+++ backend/westbook-rest/src/main/java/no/westerdals/westbook/config/SecurityConfig.java
@@ -64,6 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .logout()
                 .permitAll()
+                .logoutSuccessUrl("login.html")
                 .logoutRequestMatcher(new AntPathRequestMatcher("/rest/logout"))
                 .and()
             .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
