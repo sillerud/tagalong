4cbeb198f5f7abbd4b84fca49693540c98cde8ca
hannahsollund <hannahlizj@gmail.com>
2016-03-03 12:04:51 +0100

    movie night page

--- frontend/statisk/movie_night_page.php
+++ frontend/statisk/movie_night_page.php
@@ -3,8 +3,169 @@
 <div class="container">
     <!-- Kode her -->
 
+    <div class="card-shortcut-wrap">
+
+        <div class="card-shortcut" id="cs0">
+            <div class="cs-icon">M</div>
+            <div class="card-name">My social card is very long</div>
+        </div><br>
+        <div class="card-shortcut" id="cs1">
+            <div class="cs-icon">S</div>
+            <div class="card-name">Skolerelatert</div>
+        </div><br>
+        <div class="card-shortcut" id="cs2">
+            <div class="cs-icon">K</div>
+            <div class="card-name">Katta med slips</div>
+        </div><br>
+
+    </div><!-- END card-shortcut-wrap -->
+
+    <div class="header-image-wrap">
+        <img src="../statisk/images/movienight.jpg" class="img-responsive" alt="">
+        <div class="event-background-overlay"></div>
+        <!--<div class="page-profile-image" style="background-image: url('../statisk/images/');">
+        </div>-->
+
+        <div class="page-name"><h1>Movie Night</h1></div>
+
+        <div class="add-page-to-card">
+            <div class="add-page-to-card-text">Add to card</div>
+            <div class="add-page-to-card-btn">
+                <i class="fa fa-plus"></i>
+            </div>
+        </div>
+    </div><!-- END header-image-wrap -->
+
+    <!-- Page content -->
+    <div class="page-content">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="page-left-column">
+
+                    <div class="shout-out-box box">
+                        <div class="box-title">
+                            <h3>Movie night Shout Out</h3>
+                        </div>
+                        <div class="shout-out-text">
+                            <p>
+                                Lorem ipsum <strong>dolor sit amet</strong>, consectetur adipisicing elit. Molestiae incidunt labore sapiente.
+                            </p>
+                            <p>
+                                <strong>temporibus ab est non quia, cupiditate</strong>
+                            </p>
+                        </div>
+                    </div><!-- END shout out box -->
+
+                    <div class="about-n-gallery box">
+                        <div class="box-title">
+                            <h3><a href="">About</a></h3>
+                        </div>
+                        <span></span>
+                        <!--<div class="box-title">
+                            <h3><a href="">Gallery</a></h3>
+                        </div>-->
+                    </div><!-- END about-n-gallery -->
+
+                    <div class="about-box box">
+                        <div class="box-title">
+                            <h3>About</h3>
+                        </div>
+                        <div class="about-box-text">
+                              <div class="event-time-area">
+                                   <i class="fa fa-clock-o fa-2x event-time-icon"></i>
+                                   <p class="event-time">16.00-21.00</p>
+                               </div>
+                               
+                               <div class="event-date-area">
+                                   <i class="fa fa-calendar fa-2x event-date-icon"></i>
+                                   <p class="single-event-info event-date">Tirsdag 8. mai 2016</p>
+                               </div>
+                               
+                               <div class="event-place-area">
+                                   <i class="fa fa-map-marker fa-2x event-place event-place-icon"></i>
+                                   <p class="event-place">Westerdals, campus Galleriet</p>
+                                   <p class="event-place">Oslo 0450</p>
+                               </div>
+                               
+                            <p class="text-shown event-text">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus aut voluptatum iste? Aliquam facilis reiciendis sequi sapiente quos, ipsum voluptatum voluptate veniam consectetur assumenda doloremque quod animi fugiat, cum in...
+                            </p>
+                            <p class="text-hidden more-event-text">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis quod eveniet totam ipsum provident vitae porro pariatur distinctio modi debitis, quas fuga culpa, odit ipsa sed eum placeat non dolor!
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint velit iure cum itaque commodi assumenda deleniti laboriosam neque consequuntur aspernatur iste nihil, molestiae perspiciatis inventore dolorum est, reprehenderit magni omnis.
+                            </p>
+
+                        </div><!-- END about tekst -->
+                        <div class="about-read-more">
+                            <span class="read-more">Read more</span>
+                        </div>
+
+                    </div><!-- END about-box -->
+                    
+                    <div class="attending-box box">
+                        <div class="box-title">
+                            <h3 class="attending-box-title"><a href="">People attending</a></h3>
+                        </div>
+                        
+                        <div class="attending-thumbs-wrap">
+                            <div class="person-image" style="border: 2px solid #fd7265;">
+                                <img class="img-responsive" src="../statisk/images/profilbilde_jap.jpg" alt="">
+                            </div>
+                                    
+                            <div class="person-image" style="border: 2px solid #FED575;">
+                                <img class="img-responsive" src="images/profilbilde_2.jpg" alt="">
+                            </div>
+                        </div><!-- END attending-thumbs-wrap-->
+                                                
+                        <div class="view-all-attending">
+                            <span>See all</span>
+                        </div>
+                        
+                    </div><!-- END attending-box -->
+
+                   <!-- <div class="gallery-box box">
+                        <div class="box-title">
+                            <h3>Gallery</h3>
+                        </div>
+
+                        <div class="gallery-thumbs-wrap">
+                            <div galery-image class="gallery-thumb">
+                                <img class="thumb" src="../statisk/images/ola_nordmann.jpeg" alt="">
+                            </div>
+                            <div galery-image class="gallery-thumb">
+                                <img class="thumb" src="../statisk/images/ola_nordmann_sidebilde.jpg" alt="">
+                            </div>
+                            <div galery-image class="gallery-thumb">
+                                <img class="thumb" src="../statisk/images/event_pic.jpg" alt="">
+                            </div>
+                            <div galery-image class="gallery-thumb">
+                                <img class="thumb" src="../statisk/images/ola_nordmann_sidebilde.jpg" alt="">
+                            </div>
+                            <div galery-image class="gallery-thumb">
+                                <img class="thumb" src="../statisk/images/profilbilde_jap.jpg" alt="">
+                            </div>
+                            <div galery-image class="gallery-thumb">
+                                <img class="thumb" src="../statisk/images/ola_nordmann_sidebilde.jpg" alt="">
+                            </div>
+                        </div>
+
+                        <div class="view-gallery">
+                            <span>View gallery</span>
+                        </div>
+
+                    </div>--><!-- END about-box -->
 
 
+                </div><!-- END page-left-column -->
+            </div>
+            <div class="col-sm-6">
+                <div class="page-right-column">
+                    <!-- TODO
+                    <?php include 'person_feed.php' ?>
+                </div><!-- END page-right-column -->
+            </div><!-- END col-sm-6 -->
+        </div>
+    </div><!-- END page-content -->
 
 </div><!--end container-->
 <?php include 'footer.php'; ?>
