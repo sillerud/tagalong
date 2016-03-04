9a8557d0ce71e2f02e38ef6d97b4fd560ca7c2b1
gjermundnor <gjermundnor@outlook.com>
2016-03-04 10:29:17 +0100

    Searchbar css

---
+++ frontend/statisk/index.php
@@ -0,0 +1,120 @@
+<?php include 'header.php'; ?>
+
+<div id="darkOverlay"></div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card-wrap">
+                <div class="new-post-in-card-icon">
+                    5
+                </div>
+                <div class="edit-card-icon">
+                    <i class="fa fa-cog"></i>
+                </div>
+                <div class="card-wrap-inner">
+
+                    <h3>Tittel på Card</h3>
+                    <p>Dette kortet er sosialt og om webutvikling</p>
+
+                    <!-- Viser kun 3 tags, kan sepsifisere hvilke det er i edit card -->
+                    <div class="card-tag-wrap">
+                        <ul class="card-tag-list">
+                            <li>#Utvikling</li>
+                            <li>@Fubar</li>
+                            <li>#Sosialt</li>
+                        </ul>
+                    </div>
+                </div>
+            </div><!-- END - card-wrap -->
+        </div><!-- END col-sm-4 -->
+
+        <div class="col-sm-4">
+            <div class="card-wrap">
+                <div class="new-post-in-card-icon">
+                    20
+                </div>
+                <div class="edit-card-icon">
+                    <i class="fa fa-cog"></i>
+                </div>
+                <div class="card-wrap-inner">
+
+                    <h3>Social</h3>
+                    <p>Alt sosialt</p>
+
+                    <!-- Viser kun 3 tags, kan sepsifisere hvilke det er i edit card -->
+                    <div class="card-tag-wrap">
+                        <ul class="card-tag-list">
+                            <li>@Fubar</li>
+                            <li>#Sosialt</li>
+                        </ul>
+                    </div>
+                </div>
+            </div><!-- END - card-wrap -->
+        </div><!-- END col-sm-4 -->
+
+        <div class="col-sm-4">
+            <div class="card-wrap">
+                <div class="new-post-in-card-icon">
+                    99
+                </div>
+                <div class="edit-card-icon">
+                    <i class="fa fa-cog"></i>
+                </div>
+                <div class="card-wrap-inner">
+
+                    <h3>Tittel på Card</h3>
+                    <p>Dette kortet er sosialt og om webutvikling</p>
+
+                    <!-- Viser kun 3 tags, kan sepsifisere hvilke det er i edit card -->
+                    <div class="card-tag-wrap">
+                        <ul class="card-tag-list">
+                            <li>#Utvikling</li>
+                            <li>@Fubar</li>
+                            <li>#Sosialt</li>
+                        </ul>
+                    </div>
+                </div>
+            </div><!-- END - card-wrap -->
+        </div><!-- END col-sm-4 -->
+
+        <div class="col-sm-4">
+            <div class="card-wrap">
+                <div class="new-post-in-card-icon">
+                    0
+                </div>
+                <div class="edit-card-icon">
+                    <i class="fa fa-cog"></i>
+                </div>
+                <div class="card-wrap-inner">
+
+                    <h3>Film ++</h3>
+                    <p>Dette kortet er sosialt og om webutvikling</p>
+
+                    <!-- Viser kun 3 tags, kan sepsifisere hvilke det er i edit card -->
+                    <div class="card-tag-wrap">
+                        <ul class="card-tag-list">
+                            <li>#Render</li>
+                            <li>#TV</li>
+                            <li>#Sosialt</li>
+                        </ul>
+                    </div>
+                </div>
+            </div><!-- END - card-wrap -->
+        </div><!-- END col-sm-4 -->
+
+        <div class="col-sm-4">
+            <div class="card-wrap-add">
+                <div class="card-add-inner">
+                    <i class="fa fa-plus"></i><br>
+                    Add new card
+                </div><!-- END - card-add-inner -->
+            </div><!-- END - card-wrap -->
+        </div><!-- END col-sm-4 -->
+
+    </div><!-- END row -->
+
+</div><!-- END - container -->
+
+
+<?php include 'footer.php'; ?>
