958bb1c05322c16da37dc1e8369d0585c0666060
gjermundnor <gjermundnor@outlook.com>
2016-03-10 11:30:17 +0100

    Tag search statisk

--- frontend/angular/js/card_controllers.js
+++ frontend/angular/js/card_controllers.js
@@ -27,6 +27,16 @@ cardControllers.controller("AddCardCtrl", ['$scope', 'Card', function($scope, Ca
 
     filter.push("#java");
 
+    $scope.itemArray = [
+        {id: 1, name: 'first'},
+        {id: 2, name: 'second'},
+        {id: 3, name: 'third'},
+        {id: 4, name: 'fourth'},
+        {id: 5, name: 'fifth'},
+    ];
+
+    $scope.selected = { value: $scope.itemArray[0] };
+
     $scope.createCard = function() {
         Card.create({
             userId: $scope.me.id,
