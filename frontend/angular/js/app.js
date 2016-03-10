b896717a2ca3e8dce63639f9364ed859cc7f4478
gjermundnor <gjermundnor@outlook.com>
2016-03-10 10:30:34 +0100

    Working tag-search på pages (ikke stylet)

--- frontend/angular/js/app.js
+++ frontend/angular/js/app.js
@@ -2,6 +2,8 @@
 angular.module('tagalong', [
         'ngRoute',
         'ngFileUpload',
+        'ui.select',
+        'ngSanitize',
         'eventControllers',
         'eventServices',
         'userControllers',
