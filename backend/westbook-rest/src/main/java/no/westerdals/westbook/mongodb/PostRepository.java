ee56f6c363cf590a12f825f4f657a4a64ce4f26e
Kevin Sillerud <silkev14@student.westerdals.no>
2016-03-11 11:09:33 +0100

    tags -> tagIds

--- backend/westbook-rest/src/main/java/no/westerdals/westbook/mongodb/PostRepository.java
+++ backend/westbook-rest/src/main/java/no/westerdals/westbook/mongodb/PostRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 public interface PostRepository extends MongoRepository<Post, String>, PostRepositoryCustom
 {
     List<Post> getByUserId(String userId);
-    List<Post> getByTagsTagId(String tagId);
-    List<Post> getByTagsTagId(String tagId, Pageable pageable);
+    List<Post> getByTagsTagIds(String[] tagId);
+    List<Post> getByTagsTagIds(String[] tagId, Pageable pageable);
     List<Post> getByParentId(String pageId);
 }
