8f068ef6a697ca0e691f1ad087e1ca04418de778
Kevin Sillerud <silkev14@student.westerdals.no>
2016-03-14 21:39:34 +0100

    Dont alter, just validate images, browsers doesnt like how java encodes images

--- backend/westbook-rest/src/main/java/no/westerdals/westbook/uploads/MongoUploadService.java
+++ backend/westbook-rest/src/main/java/no/westerdals/westbook/uploads/MongoUploadService.java
@@ -2,15 +2,18 @@ package no.westerdals.westbook.uploads;
 
 import com.mongodb.gridfs.GridFSDBFile;
 import com.mongodb.gridfs.GridFSFile;
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import lombok.*;
 import no.westerdals.westbook.ImageType;
 import no.westerdals.westbook.model.FileMeta;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 
 import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.List;
@@ -40,12 +43,12 @@ public class MongoUploadService implements UploadService {
         if (imageType == null) {
             file = fsTemplate.store(in, meta.getName(), mongoFileMeta);
         } else {
-            BufferedImage image = ImageIO.read(in);
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            IOUtils.copy(in, bytes);
+            BufferedImage image = ImageIO.read(new ByteArrayInputStream(bytes.toByteArray()));
             if (image.getWidth() > imageType.getMaxWidth() || image.getHeight() > imageType.getMaxHeight())
                 return null;
-            ByteArrayOutputStream out = new ByteArrayOutputStream(); // Need to redo this
-            ImageIO.write(image, "jpg", out);
-            file = fsTemplate.store(new ByteArrayInputStream(out.toByteArray()), meta.getName(), mongoFileMeta);
+            file = fsTemplate.store(new ByteArrayInputStream(bytes.toByteArray()), meta.getName(), mongoFileMeta);
         }
         return deserialize(file);
     }
